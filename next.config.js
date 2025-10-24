3adac03a2b1e79fb4e8d37c361c60f90f4e848ce
Sundram Pandey <sundram.pandey@uttamgalva.in>
2025-10-24 13:44:30 +0530

    Fix: Remove experimental optimizeCss causing build errors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     domains: ['images.unsplash.com'],
     formats: ['image/webp', 'image/avif'],
